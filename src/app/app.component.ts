f46a23cc4023c5a3a9551f1d3c50b6d9a9913729
somikdatta <developersomik@gmail.com>
2019-07-07 20:02:19 +0530

    UI fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit {
+  private height: any;
+  constructor() {
+    // this.height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    // document.getElementsByTagName('html')[0].style.fontSize = `${this.height}px`;
+  }
+
+  ngOnInit() {
+  }
 }
